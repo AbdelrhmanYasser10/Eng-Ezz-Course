7d3f9984abf455e15d627e43e856c9ec2f676795
Abdelrhman Yasser <abdelrhmanyasser123@gmail.com>
2025-11-29 14:49:44 +0200

    finish authentication

--- lib/features/home/presentation/pages/home_page.dart
+++ lib/features/home/presentation/pages/home_page.dart
@@ -10,21 +10,39 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return BlocProvider(
       create: (context) => HomeCubit()..getUserData(),
-      child: Scaffold(
-        body: BlocConsumer<HomeCubit, HomeState>(
+      child: BlocConsumer<HomeCubit, HomeState>(
           listener: (context, state) {},
           builder: (context, state) {
             if (state is GetUserDataLoading) {
-              return LoadingWidget();
+              return Scaffold(body: LoadingWidget());
             }
-            return Center(
-              child: Text(
-                context.read<HomeCubit>().currentUser!.email.toString(),
+            return Scaffold(
+              appBar: AppBar(
+                leading: IconButton(onPressed: (){}, icon: Icon(Icons.menu)),
+                title: Image.asset(
+                    "assets/logo/logoipsum-255 1.png",
+                  width: 100,
+                  height: 100,
+                  fit: BoxFit.contain,
+                ),
+                centerTitle: true,
+                actions: [
+                  CircleAvatar(
+                    radius: 18,
+                    backgroundImage: NetworkImage(
+                      context.read<HomeCubit>().currentUser!.avatar!,
+                    ),
+                  ),
+                ],
+              ),
+              body: Center(
+                child: Text(
+                  context.read<HomeCubit>().currentUser!.email.toString(),
+                ),
               ),
             );
           },
         ),
-      ),
     );
   }
 }
